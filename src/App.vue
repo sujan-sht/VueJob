330635ee4f7f68dea1c7046169f9f16e779e2347
sujan-sht <sujanstha016@gmail.com>
2025-03-26 22:32:48 +0545

    vue job

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import Navbar from '@/components/Navbar.vue'
+  import {RouterView} from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <Navbar />
+      <RouterView />
+    </div>
+  </header>
+
+
+</template>
+
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import Hero from '@/components/Hero.vue'
+  import HomeCards from '@/components/HomeCards.vue';
+  import JobListings from '@/components/JobListings.vue';
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <Hero />
+      <HomeCards />
+      <JobListings :limit="3" :showButton="true" />
+    </div>
+  </header>
+
+</template>
