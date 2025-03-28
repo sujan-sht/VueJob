d4cd98d88d2a194b51b058bdd1ca00344a3ee9e6
sujan-sht <sujanstha016@gmail.com>
2025-03-28 13:05:25 +0545

    sweetalert added

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,131 +1,138 @@
 <script setup>
-  import { ref, onMounted } from 'vue';
-  import { RouterLink, useRoute, useRouter} from 'vue-router';
-  import { useToast } from 'vue-toastification';
-  
-  import axios from 'axios';
-  import BackButton from '@/components/BackButton.vue';
+import { ref, onMounted } from 'vue';
+import { RouterLink, useRoute, useRouter } from 'vue-router';
+import { useToast } from 'vue-toastification';
+import Swal from 'sweetalert2';
 
-  const route = useRoute();
-  const router = useRouter();
-  const toast = useToast();
-  const jobId = route.params.id;
-  const job = ref({
-    type: '',
-    title: '',
-    location: '',
+import axios from 'axios';
+import BackButton from '@/components/BackButton.vue';
+
+const route = useRoute();
+const router = useRouter();
+const toast = useToast();
+const jobId = route.params.id;
+const job = ref({
+  type: '',
+  title: '',
+  location: '',
+  description: '',
+  salary: '',
+  company: {
+    name: '',
     description: '',
-    salary: '',
-    company: {
-      name: '',
-      description: '',
-      contactEmail: '',
-      contactPhone: ''
-    }
-  });
-
-  const deleteJob = async () => {
-    try{
-      const confirm =window.confirm('Delete');
-      if(confirm){
-        await axios.delete(`/api/jobs/${jobId}`);
-        toast.success('Job deleted successfully');
-        router.push('/jobs');
-      }
-      
-    }catch(error){
-      console.log(error);
-      toast.error('Error while deleting');
-    }
+    contactEmail: '',
+    contactPhone: ''
   }
-  onMounted(async() => {
-    try{
-      const response = await axios.get(`/api/jobs/${jobId}`);
-      job.value = response.data;
-    }catch(error){
-      console.log('Error Fetching Job', error);
+});
+
+const deleteJob = async () => {
+  try {
+    const result = await Swal.fire({
+      title: 'Are you sure?',
+      text: 'You won’t be able to revert this!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it'
+    });
+
+    if (result.isConfirmed) {
+      await axios.delete(`/api/jobs/${jobId}`);
+      toast.success('Job deleted successfully');
+      router.push('/jobs');
     }
-  })
+
+    // const confirm = window.confirm('Delete');
+    // if (confirm) {
+    //   await axios.delete(`/api/jobs/${jobId}`);
+    //   toast.success('Job deleted successfully');
+    //   router.push('/jobs');
+    // }
+
+  } catch (error) {
+    console.log(error);
+    toast.error('Error while deleting');
+  }
+}
+onMounted(async () => {
+  try {
+    const response = await axios.get(`/api/jobs/${jobId}`);
+    job.value = response.data;
+  } catch (error) {
+    console.log('Error Fetching Job', error);
+  }
+})
 
 </script>
 
 <template>
   <BackButton />
-    <section class="bg-green-50">
-      <div class="container m-auto py-10 px-6">
-        <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-          <main>
-            <div
-              class="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
-            >
-              <div class="text-gray-500 mb-4">{{ job.type }}</div>
-              <h1 class="text-3xl font-bold mb-4">{{ job.title }}</h1>
-              <div
-                class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
-              >
-                <i
-                  class="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
-                ></i>
-                <p class="text-orange-700">{{ job.location }}</p>
-              </div>
+  <section class="bg-green-50">
+    <div class="container m-auto py-10 px-6">
+      <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+        <main>
+          <div class="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
+            <div class="text-gray-500 mb-4">{{ job.type }}</div>
+            <h1 class="text-3xl font-bold mb-4">{{ job.title }}</h1>
+            <div class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
+              <i class="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
+              <p class="text-orange-700">{{ job.location }}</p>
             </div>
+          </div>
 
-            <div class="bg-white p-6 rounded-lg shadow-md mt-6">
-              <h3 class="text-green-800 text-lg font-bold mb-6">
-                Job Description
-              </h3>
+          <div class="bg-white p-6 rounded-lg shadow-md mt-6">
+            <h3 class="text-green-800 text-lg font-bold mb-6">
+              Job Description
+            </h3>
 
-              <p class="mb-4">
-               {{job.description}}
-              </p>
+            <p class="mb-4">
+              {{ job.description }}
+            </p>
 
-              <h3 class="text-green-800 text-lg font-bold mb-2">Salary</h3>
+            <h3 class="text-green-800 text-lg font-bold mb-2">Salary</h3>
 
-              <p class="mb-4">{{ job.salary }} / Year</p>
-            </div>
-          </main>
+            <p class="mb-4">{{ job.salary }} / Year</p>
+          </div>
+        </main>
 
-          <!-- Sidebar -->
-          <aside>
-            <!-- Company Info -->
-            <div class="bg-white p-6 rounded-lg shadow-md">
-              <h3 class="text-xl font-bold mb-6">Company Info</h3>
+        <!-- Sidebar -->
+        <aside>
+          <!-- Company Info -->
+          <div class="bg-white p-6 rounded-lg shadow-md">
+            <h3 class="text-xl font-bold mb-6">Company Info</h3>
 
-              <h2 class="text-2xl">{{ job.company.name }}</h2>
+            <h2 class="text-2xl">{{ job.company.name }}</h2>
 
-              <p class="my-2">
-                {{ job.company.description }}
-              </p>
+            <p class="my-2">
+              {{ job.company.description }}
+            </p>
 
-              <hr class="my-4" />
+            <hr class="my-4" />
 
-              <h3 class="text-xl">Contact Email:</h3>
+            <h3 class="text-xl">Contact Email:</h3>
 
-              <p class="my-2 bg-green-100 p-2 font-bold">
-                {{ job.company.contactEmail }}
-              </p>
+            <p class="my-2 bg-green-100 p-2 font-bold">
+              {{ job.company.contactEmail }}
+            </p>
 
-              <h3 class="text-xl">Contact Phone:</h3>
+            <h3 class="text-xl">Contact Phone:</h3>
 
-              <p class="my-2 bg-green-100 p-2 font-bold">{{ job.company.contactPhone }}</p>
-            </div>
+            <p class="my-2 bg-green-100 p-2 font-bold">{{ job.company.contactPhone }}</p>
+          </div>
 
-            <!-- Manage -->
-            <div class="bg-white p-6 rounded-lg shadow-md mt-6">
-              <h3 class="text-xl font-bold mb-6">Manage Job</h3>
-              <RouterLink
-                :to="`/jobs/edit/${jobId}`"
-                class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                >Edit Job</RouterLink
-              >
-              <button @click="deleteJob"
-                class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-              >
-                Delete Job
-              </button>
-            </div>
-          </aside>
-        </div>
+          <!-- Manage -->
+          <div class="bg-white p-6 rounded-lg shadow-md mt-6">
+            <h3 class="text-xl font-bold mb-6">Manage Job</h3>
+            <RouterLink :to="`/jobs/edit/${jobId}`"
+              class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+              Edit Job</RouterLink>
+            <button @click="deleteJob"
+              class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+              Delete Job
+            </button>
+          </div>
+        </aside>
       </div>
-    </section>
+    </div>
+  </section>
 </template>
